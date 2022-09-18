5a16cd4b79b2ffecba9b0cbdabc3583aec02aacc
@Azizjon <azizjon990415@gmail.com>
2022-09-18 18:00:03 +0500

    Initial

---
+++ src/Math/NumberDevide15.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class NumberDevide15 {
+    public static void main(String[] args) {
+        for (int i = 15; i <= 100000; i++) {
+            if (i%15==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
